5a6c7cb4471a891c6dc4408b8d92d01ef4ef87dc
sakao-town <pds_ing1_sakao@outlook.fr>
2020-05-29 15:05:13 +0100

    Last commit

--- src/StageBundle/Controller/StageController.php
+++ src/StageBundle/Controller/StageController.php
@@ -3,12 +3,16 @@
 namespace StageBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use StageBundle\Entity\Traineeship;
+
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
+use UserBundle\Entity\Traineeship;
+use UserBundle\Entity\User;
+
 
 class StageController extends Controller
 {
@@ -27,41 +31,19 @@ class StageController extends Controller
         return $this->render('@Stage/Traineeship/showDetailsInternship.html.twig', ['stages'=>$stage]);
     }
 
-    public function deleteInternshipAction($id){
-        $em= $this->getDoctrine()->getManager();
-        $stage= $this->getDoctrine()->getRepository(Traineeship::class)->find($id);
-        $em->remove($stage);
-        $em->flush();
-        return $this->redirectToRoute("stage_back");
-    }
-
-
     public function editInternshipAction(Request $request,$id)
     {
         $stage= $this->getDoctrine()->getRepository(Traineeship::class)->find($id);
-
-        $stage->setCompany($stage->getCompany());
-        $stage->setStartDate($stage->getStartDate());
-        $stage->setDuration($stage->getDuration());
         $stage->setStatus($stage->getStatus());
+
         $form= $this->createFormBuilder($stage)
-            ->add('company',TextType::class, array('attr' => array('class' => 'form-control','disabled' => true),'label' => "The company"))
-            ->add('startDate',DateType::class, array('attr' => array('class' => 'form-control','disabled' => true),'widget' => 'single_text','label' => "Start date of the internship"))
-            ->add('duration', TextType::class, array('attr' => array('class' => 'form-control','disabled' => true),'label' => "The duration of the internship"))
-            ->add('status', ChoiceType::class,  array('choices' => ['Change status' => ['Valider' => 'Valider', 'Invalider' => 'Invalider','En_cours' => 'En_cours', ], ],'attr' => array('class' => 'form-control'),'label' => "Validation of the internship" ))
+            ->add('status', ChoiceType::class,  array('choices' => ['Change status' => ['Validate' => 'Validate', 'Invalidate' => 'Invalidate','In_Progress' => 'In_Progress', ], ],'attr' => array('class' => 'form-control'),'label' => "Validation of the internship" ))
             ->add('Submit', SubmitType::class, array('label' => 'Submit', 'attr' => array('class' => 'btn btn-success btn-sm','style'=>'margin-top:10px' )))
             ->getForm();
         $form->handleRequest($request);
         if ($form->isSubmitted()){
             $status= $form['status']->getData();
-            $company= $form['company']->getData();
-            $startDate= $form['startDate']->getData();
-            $duration= $form['duration']->getData();
             $em= $this->getDoctrine()->getManager();
-           // $stage= $em->getRepository(Traineeship::class)->find($id);
-            $stage->setCompany($company);
-            $stage->setStartDate($startDate);
-            $stage->setDuration($duration);
             $stage->setStatus($status);
             $em->flush();
 
@@ -70,6 +52,64 @@ class StageController extends Controller
         return $this->render("@Stage/Traineeship/editInternshipBack.html.twig",
             array("form"=>$form->createView()));
     }
+
+
+    //Front
+
+    public function serviceAction()
+    {
+        if ($this->container->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            $user = $this->container->get('security.token_storage')->getToken()->getUser();
+            $services = $this->getDoctrine()->getRepository(Traineeship::class)->findBy(array('userUser' => $user));
+
+            return $this->render("@Stage/Traineeship/allInternshipsFront.html.twig", array( 'stages'=>$services));
+        } //else
+            //return $this->render("@fidelite/front/404.html.twig");
+    }
+
+
+    public function addInternshipAction(Request $request)
+    {
+
+        $logged = $this->container->get('security.token_storage')->getToken()->getUser();
+        $user = $this->getDoctrine()->getRepository(User::class)->find($logged);
+
+        $stage= new Traineeship();
+
+        $form= $this->createFormBuilder($stage)
+            ->add('company',TextType::class, array('attr' => array('class' => 'form-control','required' => true),'label' => "Company"))
+            ->add('startDate',DateType::class, array('attr' => array('class' => 'form-control','required' => true),'widget' => 'single_text','label' => "Start date of internship"))
+            ->add('duration',TextType::class, array('attr' => array('class' => 'form-control','required' => true),'label' => "Duration of the internship"))
+            ->add('tuteur', TextType::class, array('attr' => array('class' => 'form-control','required' => true),'label' => "School tutor"))
+            ->add('missions', TextareaType::class, array('attr' => array('class' => 'form-control','required' => true),'label' => "Missions proposed by the company"))
+
+            ->add('Submit', SubmitType::class, array('label' => 'Submit', 'attr' => array('class' => 'btn btn-success btn-sm','style'=>'margin-top:10px' )))
+            ->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted()){
+            $company= $form['company']->getData();
+            $startDate= $form['startDate']->getData();
+            $duration= $form['duration']->getData();
+            $tuteur= $form['tuteur']->getData();
+            $missions= $form['missions']->getData();
+            $stage->setCompany($company);
+            $stage->setDuration($duration);
+            $stage->setStartDate($startDate);
+            $stage->setMissions($missions);
+            $stage->setTuteur($tuteur);
+            $stage->setStatus('In_Progress');
+            $stage->setUserUser($user);
+            $em= $this->getDoctrine()->getManager();
+            $em->persist($stage);
+            $em->flush();
+            $this->addFlash('message','Internship successfully added');
+            return $this->redirectToRoute("front_home");
+        }
+        return $this->render("@Stage/Traineeship/addInternshipsFront.html.twig",
+            array("form"=>$form->createView()));
+    }
+
+
 
 
 
